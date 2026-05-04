86e889a4435259dcc60cf6c05e187989ba715a3e
venkat6840 <venkat.hemanth2003@gmail.com>
2026-05-04 11:20:17 +0530

    first commit

---
+++ src/main/java/com/learningshuttle/projects/ariBnbApp/AriBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.learningshuttle.projects.ariBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriBnbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningshuttle/projects/ariBnbApp/AriBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningshuttle.projects.ariBnbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriBnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
